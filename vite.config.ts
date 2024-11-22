e6b8189da5b7fbfe0721c6534de1e3f1908e3fc1
Carlos Pacheco Aviles <carlospachecoaviles@192.168.68.103>
2024-11-22 01:14:47 -0600

    domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://inc0de.github.io/INCODE',
+  base: 'https://incodeweb.com',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
